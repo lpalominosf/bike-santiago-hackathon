d4c5ceffbb1338791664bddff49300a2ccf890ae
Lore <lorena.palominos@gmail.com>
2018-03-21 13:49:49 -0300

    componente nuevo

---
+++ src/components/vistas/vista 1/VistaUno.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class VistaUno extends Component {
+    render() {
+        return(
+            <div className="VistaUno"> 
+                <h1> Prueba </h1>
+            </div>
+        )
+    }
+}
+
+export default VistaUno;
